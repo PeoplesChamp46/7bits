d46f9fdab4e35078c7813d91bbe77c447164f539
PeoplesChamp46 <sasukemestry@gmail.com>
2022-04-02 22:45:31 +0530

    ver 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HeroComponent } from './sections/hero/hero.component';
+import { AboutUsComponent } from './sections/about-us/about-us.component';
+import { OurDreamComponent } from './sections/our-dream/our-dream.component';
+import { BrushComponent } from './sections/brush/brush.component';
+import { CustomerComponent } from './sections/customer/customer.component';
+import { ContactComponent } from './sections/contact/contact.component';
+import { ShopComponent } from './sections/shop/shop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroComponent,
+    AboutUsComponent,
+    OurDreamComponent,
+    BrushComponent,
+    CustomerComponent,
+    ContactComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/sections/our-dream/our-dream.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-our-dream',
+  templateUrl: './our-dream.component.html',
+  styleUrls: ['./our-dream.component.css']
+})
+export class OurDreamComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
